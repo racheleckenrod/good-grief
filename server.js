356d6928c00c4ee3ccc3fc4374d0fb2d5b8f2107
racheleckenrod <rachel@racheleckenrod.com>
2022-12-23 11:16:40 -0600

    still repeating 404 at socket.io transpot=polling

--- server.js
+++ server.js
@@ -1,28 +1,24 @@
-const express = require("express");
-const app = express();
-const cors = require('cors')
 const path = require("path");
 const http = require("http");
+const express = require("express");
+const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
+const app = express();
+const server = http.createServer(app);
+// const io = socketio(server);
+const cors = require('cors')
 require("dotenv").config({ path: "./config/.env" });
 const PORT = process.env.PORT;
 const PORT2 = 2899;
 app.use(cors())
 const io = require("socket.io")(PORT2, {
   cors: {
-  origin: `http://localhost:${process.env.PORT}`,
-  methods: ["GET", "POST"]
-}
+    origin: `http://localhost:${process.env.PORT}`,
+    methods: ["GET", "POST"],
+    credentials: true
+  }
 });
 
-// const io = require("socket.io")(httpServer, {
-//   cors: {
-//     origin: "http://localhost:2899",
-//     methods: ["GET", "POST"]
-//   }
-// });
-
-// httpServer.listen(3000);
-// const formatMessage = require("./utils/messages");
 const mongoose = require("mongoose");
 const passport = require("passport");
 const session = require("express-session");
@@ -37,24 +33,9 @@ const commentRoutes = require("./routes/comments");
 const chatRoutes = require("./routes/chat")
 const chatsController = require("./controllers/chats")
 
-const myURL = new URL('https://example.org/?abc=123');
-console.log(myURL.searchParams.get('abc'));
-
-
-// handle connections
-// io.on('connection', socket => {
-//   // console.log('Client connected', new Date().toTimeString());
-//   socket.emit('timeClock', "It's about time");
-//   socket.on('disconnect', () => console.log('Client disconnected'));
-// });
-
-// broadcast updates
-// let testing =  new Date().toTimeString()
-// setInterval(() => io.emit('time', `abou ${testing} time`), 1000)
-// setInterval(() => io.emit('timeData', new Date().toTimeString()), 1000);
 
 //Use .env file in config folder
-
+require("dotenv").config({ path: "./config/.env" });
 
 // Passport config
 require("./config/passport")(passport);
@@ -64,12 +45,14 @@ connectDB();
 
 
 
-//Using EJS for views
-app.set("view engine", "ejs");
 
 //Static Folder
 app.use(express.static(path.join(__dirname, "public")));
 // server.use(express.static("public"))
+
+
+//Using EJS for views
+app.set("view engine", "ejs");
 
 app.set('socketio', io);
 
@@ -101,7 +84,50 @@ app.use(passport.session());
 app.use(flash());
 
 
-// for chat
+
+
+// //Using EJS for views
+// app.set("view engine", "ejs");
+
+// //Static Folder
+// app.use(express.static(path.join(__dirname, "public")));
+// // server.use(express.static("public"))
+
+// app.set('socketio', io);
+
+// //Body Parsing
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// //Logging
+// app.use(logger("dev"));
+
+// //Use forms for put / delete
+// app.use(methodOverride("_method"));
+
+// // Setup Sessions - stored in MongoDB
+// app.use(
+//   session({
+//     secret: "goPackers",
+//     resave: false,
+//     saveUninitialized: false,
+//     store: new MongoStore({ mongooseConnection: mongoose.connection }),
+//   })
+// );
+
+// // Passport middleware
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+// //Use flash messages for errors, info, ect...
+// app.use(flash());
+
+
+
+// const createAdapter = require("@socket.io/redis-adapter").createAdapter;
+// const redis = require("redis");
+// require("dotenv").config();
+// const { createClient } = redis;
 const {
   userJoin,
   getCurrentUser,
@@ -109,17 +135,28 @@ const {
   getRoomUsers,
 } = require("./utils/users");
 
+
+
+
+// Set static folder
+app.use(express.static("public"));
+
+
 const botName = "Grief Support Bot";
 
-// run when client connects
+// (async () => {
+//   pubClient = createClient({ url: "redis://127.0.0.1:6379" });
+//   await pubClient.connect();
+//   subClient = pubClient.duplicate();
+//   io.adapter(createAdapter(pubClient, subClient));
+// })();
+
+// // Run when client connects
 io.on("connection", (socket) => {
-  const myURL = new URL('https://example.org/?abc=123');
-console.log(myURL.searchParams.get('abc'));
-  console.log('New WS Connection', socket.id, socket.handshake.headers.referer);
+  console.log('New WS Connection', socket.id,socket.handshake.headers.referer);
   socket.on("joinRoom", ({ username, room }) => {
-    console.log('test')
     const user = userJoin(socket.id, username, room);
-    console.log('gee',user)
+    console.log("pkkkkkkkk", user)
     socket.join(user.room);
 
 //     // Welcome current user
@@ -147,10 +184,9 @@ console.log(myURL.searchParams.get('abc'));
     io.to(user.room).emit("message", formatMessage(user.username, msg));
   });
 
-
-  // run when client disconnects
+//   // Runs when client disconnects
   socket.on("disconnect", () => {
-    // io.emit("message",  formatMessage(botName,'a user has left the chat'))
+    io.emit("message",  formatMessage(botName,'a user has left the chat'))
     const user = userLeave(socket.id);
 
     if (user) {
@@ -168,8 +204,8 @@ console.log(myURL.searchParams.get('abc'));
   });
 });
 
-
-
+// this route for the feedback form in the footers
+app.use("/feedback", mainRoutes);
 
 //Setup Routes For Which The Server Is Listening
 app.use("/", mainRoutes);
@@ -183,7 +219,314 @@ app.use("/chat", chatRoutes);
 // });
 
 
-//Server Running
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on PORT ${process.env.PORT}, you better catch it!`);
-});
+
+
+server.listen(PORT, () => { console.log(`Server running on port ${PORT}`)});
+
+
+
+
+
+// const express = require("express");
+// const app = express();
+// const cors = require('cors')
+// const path = require("path");
+// const http = require("http");
+// require("dotenv").config({ path: "./config/.env" });
+// const PORT = process.env.PORT;
+// const PORT2 = 2899;
+// app.use(cors())
+// const io = require("socket.io")(PORT2, {
+//   cors: {
+//     origin: `http://localhost:${process.env.PORT}`,
+//     methods: ["GET", "POST"],
+//     credentials: true
+//   }
+// });
+
+
+
+
+// // const formatMessage = require("./utils/messages");
+// const mongoose = require("mongoose");
+// const passport = require("passport");
+// const session = require("express-session");
+// const MongoStore = require("connect-mongo")(session);
+// const methodOverride = require("method-override");
+// const flash = require("express-flash");
+// const logger = require("morgan");
+// const connectDB = require("./config/database");
+// const mainRoutes = require("./routes/main");
+// const postRoutes = require("./routes/posts");
+// const commentRoutes = require("./routes/comments");
+// const chatRoutes = require("./routes/chat")
+
+
+
+// // create a Socket.io server
+// // const server = http.createServer(app);
+// // const io = socketio(server);
+
+// // handle connections
+// io.on('connection', socket => {
+//   console.log('Client connected', new Date().toTimeString());
+//   socket.emit('timeClock', "It's about time");
+//   socket.on('disconnect', () => console.log('Client disconnected'));
+// });
+
+// // broadcast updates
+// // setInterval(() => io.emit('time', "about time"), 1000)
+// // setInterval(() => io.emit('timeData', new Date().toTimeString()), 1000);
+
+// //Use .env file in config folder
+
+
+// // Passport config
+// require("./config/passport")(passport);
+
+// //Connect To Database
+// connectDB();
+
+
+
+// //Using EJS for views
+// app.set("view engine", "ejs");
+
+// //Static Folder
+// app.use(express.static(path.join(__dirname, "public")));
+// // server.use(express.static("public"))
+
+// app.set('socketio', io);
+
+// //Body Parsing
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// //Logging
+// app.use(logger("dev"));
+
+// //Use forms for put / delete
+// app.use(methodOverride("_method"));
+
+// // Setup Sessions - stored in MongoDB
+// app.use(
+//   session({
+//     secret: "goPackers",
+//     resave: false,
+//     saveUninitialized: false,
+//     store: new MongoStore({ mongooseConnection: mongoose.connection }),
+//   })
+// );
+
+// // Passport middleware
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+// //Use flash messages for errors, info, ect...
+// app.use(flash());
+
+// //Setup Routes For Which The Server Is Listening
+// app.use("/", mainRoutes);
+// app.use("/post", postRoutes);
+// app.use("/comment", commentRoutes);
+// app.use("/chat", chatRoutes);
+// // app.get("/chat",(req, res) => 
+// // render('lobby.ejs', {  }))
+
+// //Server Running
+// app.listen(process.env.PORT, () => {
+//   console.log(`Server is running on PORT ${process.env.PORT}, you better catch it!`);
+// });
+
+
+
+
+// const express = require("express");
+// const app = express();
+// const cors = require('cors')
+// const path = require("path");
+// const http = require("http");
+// require("dotenv").config({ path: "./config/.env" });
+// const PORT = process.env.PORT;
+// const PORT2 = 2899;
+// app.use(cors())
+// const io = require("socket.io")(PORT2, {
+//   cors: {
+//   origin: `http://localhost:${process.env.PORT}`,
+//   methods: ["GET", "POST"]
+// }
+// });
+
+// // const io = require("socket.io")(httpServer, {
+// //   cors: {
+// //     origin: "http://localhost:2899",
+// //     methods: ["GET", "POST"]
+// //   }
+// // });
+
+// // httpServer.listen(3000);
+// // const formatMessage = require("./utils/messages");
+// const mongoose = require("mongoose");
+// const passport = require("passport");
+// const session = require("express-session");
+// const MongoStore = require("connect-mongo")(session);
+// const methodOverride = require("method-override");
+// const flash = require("express-flash");
+// const logger = require("morgan");
+// const connectDB = require("./config/database");
+// const mainRoutes = require("./routes/main");
+// const postRoutes = require("./routes/posts");
+// const commentRoutes = require("./routes/comments");
+// const chatRoutes = require("./routes/chat")
+// const chatsController = require("./controllers/chats")
+
+// const myURL = new URL('https://example.org/?abc=123');
+// console.log(myURL.searchParams.get('abc'));
+
+
+// // handle connections
+// // io.on('connection', socket => {
+// //   // console.log('Client connected', new Date().toTimeString());
+// //   socket.emit('timeClock', "It's about time");
+// //   socket.on('disconnect', () => console.log('Client disconnected'));
+// // });
+
+// // broadcast updates
+// // let testing =  new Date().toTimeString()
+// // setInterval(() => io.emit('time', `abou ${testing} time`), 1000)
+// // setInterval(() => io.emit('timeData', new Date().toTimeString()), 1000);
+
+// //Use .env file in config folder
+
+
+// // Passport config
+// require("./config/passport")(passport);
+
+// //Connect To Database
+// connectDB();
+
+
+
+// //Using EJS for views
+// app.set("view engine", "ejs");
+
+// //Static Folder
+// app.use(express.static(path.join(__dirname, "public")));
+// // server.use(express.static("public"))
+
+// app.set('socketio', io);
+
+// //Body Parsing
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// //Logging
+// app.use(logger("dev"));
+
+// //Use forms for put / delete
+// app.use(methodOverride("_method"));
+
+// // Setup Sessions - stored in MongoDB
+// app.use(
+//   session({
+//     secret: "goPackers",
+//     resave: false,
+//     saveUninitialized: false,
+//     store: new MongoStore({ mongooseConnection: mongoose.connection }),
+//   })
+// );
+
+// // Passport middleware
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+// //Use flash messages for errors, info, ect...
+// app.use(flash());
+
+
+// // for chat
+// const {
+//   userJoin,
+//   getCurrentUser,
+//   userLeave,
+//   getRoomUsers,
+// } = require("./utils/users");
+
+// const botName = "Grief Support Bot";
+
+// // run when client connects
+// io.on("connection", (socket) => {
+//   const myURL = new URL('https://example.org/?abc=123');
+// console.log(myURL.searchParams.get('abc'));
+//   console.log('New WS Connection', socket.id, socket.handshake.headers.referer);
+//   socket.on("joinRoom", ({ username, room }) => {
+//     console.log('test')
+//     const user = userJoin(socket.id, username, room);
+//     console.log('gee',user)
+//     socket.join(user.room);
+
+// //     // Welcome current user
+//     socket.emit("message", formatMessage(botName, "Welcome to Live Grief Support!"));
+
+// //     // Broadcast when a user connects
+//     socket.broadcast
+//       .to(user.room)
+//       .emit(
+//         "message",  formatMessage(botName,`${user.username} has joined the chat`)
+// //         formatMessage(botName, `${user.username} has joined the chat`)
+//       );
+
+// //     // Send users and room info
+//     io.to(user.room).emit("roomUsers", {
+//       room: user.room,
+//       users: getRoomUsers(user.room),
+//     });
+//   });
+
+// //   // Listen for chatMessage
+//   socket.on("chatMessage", (msg) => {
+//     const user = getCurrentUser(socket.id);
+       
+//     io.to(user.room).emit("message", formatMessage(user.username, msg));
+//   });
+
+
+//   // run when client disconnects
+//   socket.on("disconnect", () => {
+//     // io.emit("message",  formatMessage(botName,'a user has left the chat'))
+//     const user = userLeave(socket.id);
+
+//     if (user) {
+//       io.to(user.room).emit(
+//         "message",
+//         formatMessage(botName, `${user.username} has left the chat`)
+//       );
+
+// //       // Send users and room info
+//       io.to(user.room).emit("roomUsers", {
+//         room: user.room,
+//         users: getRoomUsers(user.room),
+//       });
+//     }
+//   });
+// });
+
+
+
+
+// //Setup Routes For Which The Server Is Listening
+// app.use("/", mainRoutes);
+// app.use("/post", postRoutes);
+// app.use("/comment", commentRoutes);
+// app.get("/chat/:room", chatsController.getRoom);
+// app.use("/chat", chatRoutes);
+// // app.get("/chat",function(req, res, next) {
+// //   console.log("hhh",req.user.userName, req.query )
+// //   res.render('lobby.ejs', {username : req.user.userName, room: "POP"});
+// // });
+
+
+// //Server Running
+// app.listen(process.env.PORT, () => {
+//   console.log(`Server is running on PORT ${process.env.PORT}, you better catch it!`);
+// });
