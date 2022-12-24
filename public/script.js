d0bdb1c0f4392d654ad721aea09d06dc93e99d49
racheleckenrod <rachel@racheleckenrod.com>
2022-12-24 12:20:40 -0600

    replaced contents of script.js

--- public/script.js
+++ public/script.js
@@ -1,20 +1,14 @@
-
 // const io = require("socket.io");
-
-const io = require("socket.io-client");
-
 
 // const socket = io('http://localhost:3333')
 console.log("script.js")
 let timeClock ;
 
-console.log('New WS Connection', "script", "socket.connected=", socket.connected, socket.id,socket.handshake.headers.referer)});
+io.on("connection", (socket) => {
+    console.log('New WS Connection', "script", "socket.connected=", socket.connected, socket.id,socket.handshake.headers.referer)});
 
 io.on('timeClock', data => {
     console.log(data,"Personal")
-socket.on('timeClock', data => {
-    // console.log(data)
-
     timeClock = document.getElementById('time').innerHTML = data
    
 })
