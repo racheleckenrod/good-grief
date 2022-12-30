c8af2ce84ee97a1a02720a0d223fa3857c9d1c6a
racheleckenrod <rachel@racheleckenrod.com>
2022-12-30 11:39:24 -0600

    pause here

--- public/script.js
+++ public/script.js
@@ -1,18 +1,19 @@
+const socket = io();
+const id = socket.id;
 
-// const socket = io('http://localhost:3333')
-console.log("script.js")
+console.log("script.js", socket)
 let timeClock ;
 
-io.on("connection", (socket) => {
-    console.log('New WS Connection', "script", "socket.connected=", socket.connected, socket.id,socket.handshake.headers.referer)});
+socket.on("connection", (socket) => {
+    console.log('New WS SCRIPT Connection', "script", "socket.connected=", socket.connected, socket.id,socket.handshake.headers.referer)});
 
-io.on('timeClock', data => {
+socket.on('timeClock', data => {
     console.log(data,"Personal")
     timeClock = document.getElementById('time').innerHTML = data
    
 })
 
-io.on('timeData', (timeString2) => {
+socket.on('timeData', (timeString2) => {
     el = document.getElementById('currently');
     el.innerHTML = 'Current time: ' + timeString2;
 })
