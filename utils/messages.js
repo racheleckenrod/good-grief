43c4ab500b7b6ff2e8c1ff5b708108f08efa9765
racheleckenrod <rachel@racheleckenrod.com>
2023-10-24 15:02:25 -0400

    still working on the timestamp

--- utils/messages.js
+++ utils/messages.js
@@ -2,12 +2,15 @@ const moment = require('moment-timezone');
 
 function formatMessage(username, text) {
   const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  const utcTime = moment().utc();
+  console.log(userTimeZone);
+  const utcTime = moment.utc();
+  const localTime=  moment(utcTime).tz(userTimeZone).format('h:mm a');
+  console.log(utcTime, localTime);
   
   return {
     username,
     text,
-    time: moment(utcTime).tz(userTimeZone).format('h:mm a')
+    time: localTime
   };
 }
 
