805296c32b241a4526c84733748bf86ee4f193a0
racheleckenrod <rachel@racheleckenrod.com>
2023-11-07 13:36:41 -0500

    test updates

--- utils/messages.js
+++ utils/messages.js
@@ -1,10 +1,12 @@
 const moment = require('moment-timezone');
 
-function formatMessage(username, text, userTimeZone) {
+function formatMessage(username, text, userTimeZone, userLang) {
   // const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
   // console.log(userTimeZone);
   // const utcTime = moment.utc();
-  const localTime=  moment.tz(userTimeZone).format('h:mm:ss a');
+  // const localTime=  moment.tz(userTimeZone).format('h:mm:ss a');
+  const postingTime = new Date();
+  const localTime = postingTime.toLocaleString( userLang, {timeZone: userTimeZone } )
 
   
   return {
